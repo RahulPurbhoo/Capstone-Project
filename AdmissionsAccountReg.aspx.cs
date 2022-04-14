6912761b6e1d57ed44acc1eb85858473f105369d
RahulPurbhoo <99993620+RahulPurbhoo@users.noreply.github.com>
2022-04-14 09:06:00 +0200

    Admin

--- AdmissionsAccountReg.aspx.cs
+++ AdmissionsAccountReg.aspx.cs
@@ -31,28 +31,35 @@ namespace Final_Project___2022
 
         protected void btnRegister_Click(object sender, EventArgs e)
         {
-            string encryptedPassword = Encrypt(txtPassword.Text);
+            try
+            {
+                string encryptedPassword = Encrypt(txtPassword.Text);
 
-            string mainconn = ConfigurationManager.ConnectionStrings["Mysqlconnection"].ConnectionString;
-            MySqlConnection con = new MySqlConnection(mainconn);
+                string mainconn = ConfigurationManager.ConnectionStrings["Mysqlconnection"].ConnectionString;
+                MySqlConnection con = new MySqlConnection(mainconn);
 
-            con.Open();
-            //SQL Query to login
-            String Query = "INSERT INTO AdmissionsOfficer(Admissions_Number, Admissions_Name, Admissions_Surname, Admissions_IDNum, Admissions_DOB, Admissions_Password) VALUES(@Admissions_ID, @Admissions_Name, @Admissions_Surname, @Admissions_IDNum, @Admissions_DOB, @Admissions_Password);";
-            MySqlCommand cmd = new MySqlCommand(Query, con);
+                con.Open();
+                //SQL Query to login
+                String Query = "INSERT INTO AdmissionsOfficer(Admissions_Number, Admissions_Name, Admissions_Surname, Admissions_IDNum, Admissions_DOB, Admissions_Password) VALUES(@Admissions_ID, @Admissions_Name, @Admissions_Surname, @Admissions_IDNum, @Admissions_DOB, @Admissions_Password);";
+                MySqlCommand cmd = new MySqlCommand(Query, con);
 
-            cmd.Parameters.AddWithValue("@Admissions_ID", txtStdNum.Text);
-            cmd.Parameters.AddWithValue("@Admissions_Name", txtName.Text);
-            cmd.Parameters.AddWithValue("@Admissions_Surname", txtSurname.Text);
-            cmd.Parameters.AddWithValue("@Admissions_IDNum", txtIDNum.Text);
-            cmd.Parameters.AddWithValue("@Admissions_DOB", txtDOB.Text);
-            cmd.Parameters.AddWithValue("@Admissions_Password", encryptedPassword);
+                cmd.Parameters.AddWithValue("@Admissions_ID", txtStdNum.Text);
+                cmd.Parameters.AddWithValue("@Admissions_Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@Admissions_Surname", txtSurname.Text);
+                cmd.Parameters.AddWithValue("@Admissions_IDNum", txtIDNum.Text);
+                cmd.Parameters.AddWithValue("@Admissions_DOB", txtDOB.Text);
+                cmd.Parameters.AddWithValue("@Admissions_Password", encryptedPassword);
 
-            MySqlDataReader reader = cmd.ExecuteReader();
+                MySqlDataReader reader = cmd.ExecuteReader();
 
-            con.Close();
+                con.Close();
 
-            Response.Write("<script>alert('Account has been Successfully created');</script>");
+                Response.Write("<script>alert('Account has been Successfully created');</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Missing Details');</script>");
+            }
         }
     }
 }
